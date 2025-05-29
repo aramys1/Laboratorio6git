f856dfdcbb795cbcda0157cfbd5ba0afad52aa9e
aramys1 <aramysc790@gmail.com>
2025-05-29 17:08:45 -0500

    impresion en el main

--- src/Laboratorio6/Main.java
+++ src/Laboratorio6/Main.java
@@ -1,5 +1,18 @@
 package Laboratorio6;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) {
+        
+        bufferedReader bufer = new bufferedReader(new InputStreamReader(System.in));
+
+        try{
+            System.out.println("Que problema desea realizar?");
+            System.out.println("1. Calcular el factorial de un numero \n2. Calcular la suma de los divisores de un numero\n3. Calcular la cantidad de numeros primos que hay entre dos numeros \n4. Encontrar el numero mas grande de una serie de numeros ingresados por el usuario");
+        }
+        
+    }
 }
