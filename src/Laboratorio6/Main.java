9e5308ea8b7e45c4efee66260f09ac4f63a2a3b9
aramys1 <aramysc790@gmail.com>
2025-05-29 17:57:43 -0500

    case main

--- src/Laboratorio6/Main.java
+++ src/Laboratorio6/Main.java
@@ -7,14 +7,70 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         
-        bufferedReader bufer = new bufferedReader(new InputStreamReader(System.in));
+        BufferedReader bufer = new BufferedReader(new InputStreamReader(System.in));
+        int opcionesMenu = 0;
+        int factorial = 0;
+        long sumaDigitos = 0;
+        
 
-        try{
+        do{
+            try{
             System.out.println("Que problema desea realizar?");
-            System.out.println("1. Calcular el factorial de un numero \n2. Calcular la suma de los divisores de un numero\n3. Calcular la cantidad de numeros primos que hay entre dos numeros \n4. Encontrar el numero mas grande de una serie de numeros ingresados por el usuario");
-        }catch(IOException e){
-            System.out.println("Error al leer la entrada: ");
-        }
+            System.out.println("1. Calcular el factorial de un numero \n2. Calcular la suma de los divisores de un numero\n3. Calcular la cantidad de numeros primos que hay entre dos numeros \n4. Encontrar el numero mas grande de una serie de numeros ingresados por el usuario \n0. Salir");
+            opcionesMenu = Integer.parseInt(bufer.readLine());
+
+            }catch(IOException e){
+                System.out.println("Error al leer la entrada: ");
+            }
+            catch(NumberFormatException e){
+                System.out.println("Error en el formato de entrada");
+            }
+
+            switch (opcionesMenu){
+                case 1:
+                    System.out.println("Digite el numero al que desea calcular el factorial: ");
+                    try{
+                        factorial = Integer.parseInt(bufer.readLine());
+
+                    }
+                    catch(IOException e){
+                        System.out.println("error al leer la entrada");
+                    }
+                    catch(NumberFormatException e){
+                     System.out.println("Error en el formato de entrada");
+                    }
+
+                    break;
+                case 2:
+                    System.out.println("Digite el numero del cual quiera la suma de sus digitos");
+                    try{
+                        sumaDigitos = Integer.parseInt(bufer.readLine());
+
+                    }
+                    catch(IOException e){
+                        System.out.println("error al leer la entrada");
+                    }
+                    catch(NumberFormatException e){
+                     System.out.println("Error en el formato de entrada");
+                    }
+
+                    break;
+                
+                case 3:
+                    try{
+                        System.out.println("Ingrese el limite inferior");
+                        
+                    }
+            }
+
+
+
+            if(opcionesMenu == 0){
+                break;
+            }
+        
+        }while(true);
+        
         
     }
 }
