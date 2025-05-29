348b1e447dd0f7ac2fa00959fe82d886cfee8a2d
aramys1 <aramysc790@gmail.com>
2025-05-29 17:20:54 -0500

    catch agora si

--- src/Laboratorio6/Main.java
+++ src/Laboratorio6/Main.java
@@ -12,6 +12,8 @@ public class Main {
         try{
             System.out.println("Que problema desea realizar?");
             System.out.println("1. Calcular el factorial de un numero \n2. Calcular la suma de los divisores de un numero\n3. Calcular la cantidad de numeros primos que hay entre dos numeros \n4. Encontrar el numero mas grande de una serie de numeros ingresados por el usuario");
+        }catch(IOException e){
+            System.out.println("Error al leer la entrada: ");
         }
         
     }
