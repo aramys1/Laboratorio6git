d19810435ebe9bd9ad9e2ec8c5cb815fcd14ff96
(SamirHorna29) <(29hornasamir@gmail.com>
2025-05-31 17:22:03 -0500

    problema1 prueba

--- src/Laboratorio6/problema1.java
+++ src/Laboratorio6/problema1.java
@@ -1,4 +1,18 @@
 package Laboratorio6;
 
 public class problema1 {
+    private int numero;
+
+    public factorial (int numero){
+        this.numero = numero;   
+    }
+
+    public long calcular(){
+        int i;
+        long resultado = 1;
+        for (int i = 1; i <= numero; i++ ){
+            resultado = resultado*i;
+        }
+        return resultado;
+    }
 }
