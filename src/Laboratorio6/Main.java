4575512426e8e59f7b3cdb8db23d95f7974d428b
Saintdiegx <diegoxy220@gmail.com>
2025-06-01 17:23:59 -0500

    Sin comentario de prueba (Final)

--- src/Laboratorio6/Main.java
+++ src/Laboratorio6/Main.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-//comentario de prueba
+
 public class Main {
     public static void main(String[] args) {
         
