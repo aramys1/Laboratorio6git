f922e81832993727af17ded68ab5c8c16307333e
Saintdiegx <diegoxy220@gmail.com>
2025-05-31 20:01:35 -0500

    Problema 3 progreso 1

--- src/Laboratorio6/problema3.java
+++ src/Laboratorio6/problema3.java
@@ -2,7 +2,34 @@ package Laboratorio6;
 
 public class problema3 {
     //Bloque declarativo de atributos
-    private double numerosPrimos;
-    private double n1;
-    private double n2;
+    private int numerosPrimos;
+    private int sumaNumerosPrimos;
+    private int limiteInferior;
+    private int limiteSuperior;
+    private int limiteInferiorImprimir;
+    private boolean primo;
+
+
+    //Bloque de Instrccionesc
+    public void calcularNumerosPrimos(int limiteInferior, int limiteSuperior) {
+        while (limiteInferior < limiteSuperior -1) {
+            limiteInferior++;
+            limiteInferiorImprimir = limiteInferior;
+            primo = true;
+            for(int i = 2; i <= Math.sqrt(limiteInferior); i++) {
+                if (limiteInferior % i == 0) {
+                    primo = false;
+                    break;
+                }
+            }
+            if (primo) {
+                System.out.println(limiteInferiorImprimir);
+                sumaNumerosPrimos += limiteInferiorImprimir;
+                numerosPrimos++;
+            }
+        }
+    }
+
+    
+    
 }
